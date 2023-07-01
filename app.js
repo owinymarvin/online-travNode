29cbf55853978bbc60375421650610bce24415a3
timothy marvin <timothy@equipstreams.equipstreams>
2023-07-01 13:39:20 +0300

    added a few lines of code. time to divert first understand what im doing.

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars'); 
+const connectDB = require('./config/db');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-const PORT = process.env.PORT || 5000
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+const PORT = process.env.PORT || 3000
 
 app.listen(
     PORT,
